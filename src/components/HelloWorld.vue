8f4ef54b08e8b0deaeea668a0af2448f5d932482
patara <patara@exciteholidays.com>
2019-10-11 23:25:22 +0700

    make static work on vue template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,246 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+
+    <div class="search-wrapper section-padding-100">
+      <div class="search-close">
+        <i class="fa fa-close" aria-hidden="true"></i>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="search-content">
+              <form action="#" method="get">
+                <input type="search" name="search" id="search" placeholder="Type your keyword...">
+                <button type="submit"><img src="static/img/core-img/search.png" alt=""></button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-content-wrapper d-flex clearfix">
+
+      <!-- Mobile Nav (max width 767px)-->
+      <div class="mobile-nav">
+        <!-- Navbar Brand -->
+        <div class="amado-navbar-brand">
+          <a href="index.html"><img src="static/img/core-img/logo.png" alt=""></a>
+        </div>
+        <!-- Navbar Toggler -->
+        <div class="amado-navbar-toggler">
+          <span></span><span></span><span></span>
+        </div>
+      </div>
+
+      <!-- Header Area Start -->
+      <header class="header-area clearfix">
+        <!-- Close Icon -->
+        <div class="nav-close">
+          <i class="fa fa-close" aria-hidden="true"></i>
+        </div>
+        <!-- Logo -->
+        <div class="logo">
+          <a href="index.html"><img src="static/img/core-img/logo.png" alt=""></a>
+        </div>
+        <!-- Amado Nav -->
+        <!--            <nav class="amado-nav">-->
+        <!--                <ul>-->
+        <!--                    <li class="active"><a href="index.html">Home</a></li>-->
+        <!--                    <li><a href="shop.html">Shop</a></li>-->
+        <!--                    <li><a href="product-details.html">Product</a></li>-->
+        <!--                    <li><a href="cart.html">Cart</a></li>-->
+        <!--                    <li><a href="checkout.html">Checkout</a></li>-->
+        <!--                </ul>-->
+        <!--            </nav>-->
+        <!-- Button Group -->
+        <!--            <div class="amado-btn-group mt-30 mb-100">-->
+        <!--                <a href="#" class="btn amado-btn mb-15">%Discount%</a>-->
+        <!--                <a href="#" class="btn amado-btn active">New this week</a>-->
+        <!--            </div>-->
+        <!-- Cart Menu -->
+        <div class="cart-fav-search mb-100">
+          <a href="cart.html" class="cart-nav"><img src="static/img/core-img/home.png" alt=""> Home </a>
+          <a href="cart.html" class="cart-nav"><img src="static/img/core-img/cart.png" alt=""> Cart <span>(0)</span></a>
+          <a href="#" class="fav-nav"><img src="static/img/core-img/favorites.png" alt=""> Favourite</a>
+          <a href="#" class="search-nav"><img src="static/img/core-img/search.png" alt=""> Search</a>
+        </div>
+        <!-- Social Button -->
+        <div class="social-info d-flex justify-content-between">
+          <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+      </header>
+      <!-- Header Area End -->
+
+      <!-- Product Catagories Area Start -->
+      <div class="products-catagories-area clearfix">
+        <div class="amado-pro-catagory clearfix">
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/1.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $180</p>
+                <h4>Modern Chair</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/2.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $180</p>
+                <h4>Minimalistic Plant Pot</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/3.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $180</p>
+                <h4>Modern Chair</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/4.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $180</p>
+                <h4>Night Stand</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/5.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $18</p>
+                <h4>Plant Pot</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/6.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $320</p>
+                <h4>Small Table</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/7.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $318</p>
+                <h4>Metallic Chair</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/8.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $318</p>
+                <h4>Modern Rocking Chair</h4>
+              </div>
+            </a>
+          </div>
+
+          <!-- Single Catagory -->
+          <div class="single-products-catagory clearfix">
+            <a href="shop.html">
+              <img src="static/img/bg-img/9.jpg" alt="">
+              <!-- Hover Content -->
+              <div class="hover-content">
+                <div class="line"></div>
+                <p>From $318</p>
+                <h4>Home Deco</h4>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- Product Catagories Area End -->
+    </div>
+
+    <footer class="footer_area clearfix">
+      <div class="container">
+        <div class="row align-items-center">
+          <!-- Single Widget Area -->
+          <div class="col-12 col-lg-4">
+            <div class="single_widget_area">
+              <!-- Logo -->
+              <div class="footer-logo mr-50">
+                <a href="index.html"><img src="static/img/core-img/logo2.png" alt=""></a>
+              </div>
+              <!-- Copywrite Text -->
+              <p class="copywrite"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy; All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+          </div>
+          <!-- Single Widget Area -->
+          <div class="col-12 col-lg-8">
+            <div class="single_widget_area">
+              <!-- Footer Menu -->
+              <div class="footer_menu">
+                <nav class="navbar navbar-expand-lg justify-content-end">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#footerNavContent" aria-controls="footerNavContent" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                  <div class="collapse navbar-collapse" id="footerNavContent">
+                    <ul class="navbar-nav ml-auto">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="index.html">Home</a>
+                      </li>
+                      <!--                                        <li class="nav-item">-->
+                      <!--                                            <a class="nav-link" href="shop.html">Shop</a>-->
+                      <!--                                        </li>-->
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
   </div>
 </template>
 
@@ -96,18 +257,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
